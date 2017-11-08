dd0bb3e42f9e0b92494c9c5e80f46b6dcfe2d444
shaoyugong <1114730486@qq.com>
2017-11-08 15:23:00 +0800

    update

---
+++ app/src/main/java/gongsy/example/com/course/MainActivity.java
@@ -0,0 +1,24 @@
+package gongsy.example.com.course;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        Button Single=(Button)findViewById(R.id.SingleMatch);
+        Single.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,SingleMatch.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/gongsy/example/com/course/SingleMatch.java
@@ -0,0 +1,19 @@
+package gongsy.example.com.course;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Gongsy on 2017/11/8.
+ */
+
+public class SingleMatch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_match);
+    }
+}
